062431259e94b34544597e891196de30b6ae1f4f
[Students] Aleksandrs Ciula <92DPACiula@rvt.lv>
2020-10-16 11:35:21 +0300

    0.4.1

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template('home.html')
 
+@app.route('/about')
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 app.run(host='0.0.0.0', port=80, debug=True)
