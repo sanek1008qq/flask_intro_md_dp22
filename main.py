0e51e612b584fcb38f0031ede06c73613664b972
[Students] Aleksandrs Ciula <92DPACiula@rvt.lv>
2020-10-16 11:12:21 +0300

    0.4

--- main.py
+++ main.py
@@ -6,4 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+def about():
+    return render_template('about.html')
+
+def contact():
+    return render_template('contact.html')
+
 app.run(host='0.0.0.0', port=80, debug=True)
