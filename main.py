5e867cf2a51128a8c258f9f316774ff6f14d278d
[Students] Aleksandrs Ciula <92DPACiula@rvt.lv>
2020-10-16 10:50:08 +0300

    0.3.2

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>bruh1</h1>"
+    return render_template('home.html')
 
 app.run(host='0.0.0.0', port=80, debug=True)
